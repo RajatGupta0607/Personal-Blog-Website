eddc6e9af3ce6f66910bd091cc56d4052e87dc04
NightShift0607 <142142387+NightShift0607@users.noreply.github.com>
2024-03-07 12:18:42 +0530

    Read Data Completed And Pagination to be implemented

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,19 +3,28 @@ import { db } from "../config/db.js";
 const router = express.Router();
 
 // Routes
-router.get("/", (req, res) => {
-  res.render("index");
+
+// Home Page Route
+router.get("/", async (req, res) => {
+  try {
+    const data = (await db.query("SELECT * FROM articles")).rows;
+    res.render("index", { data });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
+// About Page Route
 router.get("/about", (req, res) => {
   res.render("about");
 });
 
+// Contact Page Route
 router.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-router.get("/article", (req, res) => {
+router.get("/article/:id", (req, res) => {
   res.render("article");
 });
 
